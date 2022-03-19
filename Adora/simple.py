3f318af3cfac2c5c68a1f4bbac4e82013821348e
adora2211 <95846872+adora2211@users.noreply.github.com>
2022-03-19 16:32:27 +0530

    Create simple.py

---
+++ Adora/simple.py
@@ -0,0 +1,46 @@
+#code
+#print
+#variable
+#operator
+#number (int,float,long)
+# letter (char,string)
+
+
+
+
+
+#area and perimeter of square
+
+
+print("Hello World")
+print("Nivedita")
+
+# wap area of rectangle
+
+# length,breadth (length*bradth)
+
+length = int(input())
+
+breadth = int(input())
+
+area = length * breadth
+
+print(area)
+print("area")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
